887a5f7bb339fcb7c177b8de87ddd14e4a9808d8
tejas31007 <tej.alawani@gmail.com>
2025-12-07 15:31:53 +0530

    config: add redis host and queue settings

--- proxy/config.py
+++ proxy/config.py
@@ -1,8 +1,13 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    # CHANGED: Now pointing to our local Java app
+    # Target (The Java Victim)
     TARGET_URL: str = "http://127.0.0.1:8080"
+
+    # Database (The Brain)
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    REDIS_QUEUE_NAME: str = "traffic_log"  # The name of our list
 
     class Config:
         env_file = ".env"
