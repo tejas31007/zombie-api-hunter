45cf64b07133050d74b02d34f10039e3cddfc656
tejas31007 <tej.alawani@gmail.com>
2025-12-16 09:41:18 +0530

    refactor: update config to support docker environment variables

--- proxy/config.py
+++ proxy/config.py
@@ -1,14 +1,17 @@
+import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # Target (The Java Victim)
-    TARGET_URL: str = "http://127.0.0.1:8080"
-    
+    # If running in Docker, we use 'host.docker.internal' to reach your laptop
+    TARGET_URL: str = os.getenv("TARGET_URL", "http://host.docker.internal:8080")
+
     # Redis (The Brain & Memory)
-    REDIS_HOST: str = "localhost"
+    # We check for an Env Var 'REDIS_HOST'. If not found, default to 'localhost'
+    REDIS_HOST: str = os.getenv("REDIS_HOST", "localhost")
     REDIS_PORT: int = 6379
     REDIS_QUEUE_NAME: str = "traffic_logs"
-    
+
     # Rate Limiting (The Speed Limit)
     RATE_LIMIT_COUNT: int = 5       # Max requests
     RATE_LIMIT_WINDOW: int = 60     # Per X seconds
